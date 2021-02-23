6581f99031eb28238673b5533effc6c5b74aeec0
max10010 <maxmachado.x@gmail.com>
2021-02-23 11:41:44 -0300

    Validando demais itens da rota Signup

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -1,18 +1,81 @@
-require "httparty"
+require_relative "routes/sessions"
+require_relative "helpers"
 
 describe "POST /sessions" do
-  it "login com sucesso" do
-    payload = { email: "max@gmail.com", password: "mudar123" }
+  context "login com sucesso" do
+    before(:all) do
+      payload = { email: "max@gmail.com", password: "mudar123" }
+      @result = Sessions.new.login(payload)
+    end
 
-    result = HTTParty.post(
-      "http://rocklov-api:3333/sessions",
-      body: payload.to_json,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    )
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
 
-    expect(result.code).to eql 200
-    expect(result.parsed_response["_id"].length).to eql 24
+    it "valida id do usuário" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+
+  # examples = [
+  #   {
+  #     title: "senha incorreta",
+  #     payload: { email: "max@gmail.com", password: "mudar12q3" },
+  #     code: 401,
+  #     error: "Unauthorized",
+  #   },
+
+  #   {
+  #     title: "usuario nao existe",
+  #     payload: { email: "404@gmail.com", password: "mudar123" },
+  #     code: 401,
+  #     error: "Unauthorized",
+  #   },
+
+  #   {
+  #     title: "email em branco",
+  #     payload: { email: "", password: "mudar123" },
+  #     code: 412,
+  #     error: "required email",
+  #   },
+
+  #   {
+  #     title: "sem o campo email",
+  #     payload: { password: "mudar123" },
+  #     code: 412,
+  #     error: "required email",
+  #   },
+
+  #   {
+  #     title: "sem em branco",
+  #     payload: { email: "max@gmail.com", password: "" },
+  #     code: 412,
+  #     error: "required password",
+  #   },
+
+  #   {
+  #     title: "sem o campo senha",
+  #     payload: { email: "max@gmail.com" },
+  #     code: 412,
+  #     error: "required password",
+  #   },
+  # ]
+
+  examples = Helpers::get_fixture("login")
+
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        @result = Sessions.new.login(e[:payload])
+      end
+
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "valida id do usuário" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
+    end
   end
 end
---
+++ api/spec/post_signup_spec.rb
@@ -0,0 +1,68 @@
+require_relative "routes/signup"
+require_relative "libs/mongo"
+require_relative "helpers"
+
+describe "POST /signup" do
+  context "novo usuario" do
+    before(:all) do
+      payload = { name: "Pitty", email: "pitty@bol.com", password: "mudar123" }
+      MongoDB.new.remove_user(payload[:email])
+
+      @result = Signup.new.create(payload)
+    end
+
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+
+  context "usuario ja existe" do
+    before(:all) do
+      # dado que eu tenho um novo usuário
+      payload = { name: "Joao da Silva", email: "joao@bol.com", password: "mudar123" }
+      MongoDB.new.remove_user(payload[:email])
+
+      # e o email desse usuário já foi cadastrado no sistema
+      Signup.new.create(payload)
+
+      # quando faço uma requisição para rota /signup
+      @result = Signup.new.create(payload)
+    end
+
+    it "deve retornar 409" do
+      # entao deve retornar 409
+      expect(@result.code).to eql 409
+    end
+
+    it "deve retornar mensagem" do
+      expect(@result.parsed_response["error"]).to eql "Email already exists :("
+    end
+  end
+
+  # name é obrigatório
+  # email é obrigatório
+  # password é obrigatório
+
+  examples = Helpers::get_fixture("signup")
+
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        MongoDB.new.remove_user(e[:email])
+        @result = Signup.new.create(e[:payload])
+      end
+
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "deve retornar mensagem de erro" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
+    end
+  end
+end
